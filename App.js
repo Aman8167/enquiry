846ca630f13aa3be7bbb07f283c3c0338dda24a2
Aman8167 <118344734+Aman8167@users.noreply.github.com>
2023-08-09 15:16:28 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css';
+import Enquiry from './Component/Enquiry';
+
+
+
+function App() {
+  return (
+     
+      <div className='app'>
+  <Enquiry />
+      </div>
+
+  );
+}
+
+export default App;
---
+++ Enquiry.js
@@ -0,0 +1,93 @@
+import React from 'react'
+import './Enquiry.css';
+function Enquiry() {
+  return (
+    <div className="container-fluid">
+      <br></br>
+    <div className="row justify-content-center">
+
+      <div className="col-md-12">
+        <div className="custom-card ml-4">
+        <div class="row">
+        <div className="col-sm-5">
+               <div class="card-body">
+                <p>
+                  Enquiry id -#12<br></br><br></br><br></br>
+                  Name-
+                </p>
+               </div>
+            </div>
+            <div className="col-sm-5">
+               <div class="card-body1">
+               <button type="button" class="btn btn-warning">View</button>
+               </div>
+            </div>
+        </div>
+          
+        </div>
+      </div>
+      
+ 
+      
+    </div>
+    <br></br><br>
+    </br>
+    <div className="row justify-content-center">
+
+      <div className="col-md-12">
+        <div className="custom-card ml-4">
+        <div class="row">
+        <div className="col-sm-5">
+               <div class="card-body">
+                <p>
+                Enquiry id -#12<br></br><br></br><br></br>
+                  Name-
+                </p>
+               </div>
+            </div>
+            <div className="col-sm-5">
+               <div class="card-body1 ">
+               <button type="button" class="btn btn-warning">View</button>
+               </div>
+            </div>
+        </div>
+          
+        </div>
+      </div>
+      
+ 
+      
+    </div>
+<br></br>
+    <div className="row justify-content-center">
+
+<div className="col-md-12">
+  <div className="custom-card ml-4">
+  <div class="row">
+  <div className="col-sm-5">
+         <div class="card-body">
+          <p>
+          Enquiry id -#12<br></br><br></br><br></br>
+            Name-
+          </p>
+         </div>
+      </div>
+      <div className="col-sm-5">
+         <div class="card-body1 ">
+         <button type="button" class="btn btn-warning">View</button>
+         </div>
+      </div>
+  </div>
+    
+  </div>
+</div>
+
+
+
+</div>
+    
+  </div>
+  )
+}
+
+export default Enquiry
